4ae8e4eaa076c49df13aa0f403bc5ad34bfae3a7
pedropaulocg <pedropaulocostaguima@hotmail.com>
2022-07-18 13:36:15 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+new Vue({
+    el: '#app',
+    data: {
+        chips: [{apelido:'Mala pessoal',serie:'BR786428',color:'#E33348',ativo: true},],
+        newApelido: '',
+        newSerial: '',
+        newColor: ''
+    },
+    methods: {
+        addChip () {
+            let apelidoStore = this.newApelido
+            let serialStore = this.newSerial
+            let colorStore = this.newColor
+
+            this.chips.push({apelido: apelidoStore, serie: serialStore, color: colorStore, ativo: false})
+            this.newApelido = ''
+            this.newColor = ''
+            this.newSerial = ''
+        }
+    }
+})
